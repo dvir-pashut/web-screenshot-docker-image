62ce653d2261413dcffa215a04a5b010d7df0528
Ubuntu <ubuntu@tests.1oju0fvszq1ujmvfdfgfkgyqfh.fx.internal.cloudapp.net>
2023-05-25 12:31:21 +0000

    first version is working

--- app.py
+++ app.py
@@ -1,22 +1,70 @@
 '''
-this is a python script
+
+Capture a screenshot of the webpage at the given URL and save it locally.
+
+syntax:
+    python3 app.py <url>
+
+Returns:
+    None
+
 '''
 import sys
+import requests
 from time import sleep
 from pyvirtualdisplay import Display
 from selenium import webdriver
 
 
-URL = sys.argv[1]
+def is_valid_url(url):
+    '''
+    Check if the URL is valid by sending a HEAD request.
 
-display = Display(visible=0, size=(1920, 1080))
-display.start()
+    Args:
+        url (str): The URL to check.
+
+    Returns:
+        bool: True if the URL is valid, False otherwise.
+    '''
+    try:
+        response = requests.head(url)
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
+
+def screenshot(url):
+    '''
+    Capture a screenshot of the webpage at the given URL.
+
+    Args:
+        url (str): The URL of the webpage to capture.
+
+    Returns:
+        None
+
+    '''
+    if not is_valid_url(url):
+        print("Invalid URL:", url)
+        return
+
+    display = Display(visible=0, size=(1920, 1080))
+    display.start()
+
+    driver = webdriver.Firefox()
+    driver.get(url)
+    sleep(1)
+
+    driver.get_screenshot_as_file("screenshot.png")
+    driver.quit()
 
 
-driver = webdriver.Firefox()
-driver.get(URL)
-sleep(1)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide a URL as an argument.")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        print("please provide only one URL")
+        sys.exit(1)
+    URL = sys.argv[1]
 
-driver.get_screenshot_as_file("screenshot.png")
-driver.quit()
-print("end...")
+    screenshot(URL)
