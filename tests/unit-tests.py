b7ccbc257fd25dfdac80e4a5ab44f0ff6d6d2be9
Ubuntu <ubuntu@tests.1oju0fvszq1ujmvfdfgfkgyqfh.fx.internal.cloudapp.net>
2023-05-25 15:09:44 +0000

    testing slack integration

---
+++ tests/unit-tests.py
@@ -0,0 +1,36 @@
+import pytest
+from app import is_valid_url, screenshot
+
+@pytest.mark.parametrize("url, expected", [
+    ("https://www.example.com", True),
+    ("http://invalidurl", False),
+    ("https://www.google.com", True),
+    ("ftp://example.com", False)
+])
+def test_is_valid_url(url, expected):
+    assert is_valid_url(url) == expected
+
+
+@pytest.mark.parametrize("url", [
+    "https://www.example.com",
+    "https://www.google.com",
+    "https://www.github.com"
+])
+def test_screenshot(url):
+    screenshot(url)
+    # Perform additional assertions if necessary
+
+
+def test_screenshot_invalid_url():
+    with pytest.raises(SystemExit):
+        screenshot("http://invalidurl")
+
+
+def test_screenshot_no_arguments():
+    with pytest.raises(SystemExit):
+        screenshot()
+
+
+def test_screenshot_multiple_arguments():
+    with pytest.raises(SystemExit):
+        screenshot("https://www.example.com", "https://www.google.com")
